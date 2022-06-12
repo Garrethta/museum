1e24fc019abd832e1a183a0af41a8cd56e8b3d41
Garrethta <degtyarev2000@inbox.ru>
2022-06-12 17:00:55 +0300

    feat: hasrouter

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import {
   BrowserRouter,
   Routes,
   Route,
+  HashRouter
 } from "react-router-dom";
 import { HeaderComponent } from './HeaderComponent';
 import { FooterComponent } from './FooterComponent';
@@ -16,15 +17,15 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between',minHeight:'100vh' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', minHeight: '100vh' }}>
       <HeaderComponent />
-      <BrowserRouter>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <Routes>
-          <Route index element={<HomeComponent />} />
+          <Route path={`/`} element={<HomeComponent />} />
           <Route path={`/old/:id`} element={<MaterialComponent mode='old' />} />
           <Route path={`/new/:id`} element={<MaterialComponent mode='new' />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       <FooterComponent />
     </div>
   </React.StrictMode>
